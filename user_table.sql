ec9a44b8d0a937f73959cfdc1b21802a96cba24c
Gabrieljev <gabriel.evangeli@tiket.com>
2022-12-13 14:51:48 +0700

    udpating user table

--- user_table.sql
+++ user_table.sql
@@ -1,5 +1,3 @@
 INSERT INTO book_store.`user` (username,password,created_at,updated_at,is_deleted,deleted_at) VALUES 
-('geb','$2a$10$gSGzSQNZNMHu2F3JB7x.beomYOjjLPukP07teMehamGYpbPmh2lHK','2022-11-30 23:32:34','2022-11-30 23:32:34',0,NULL)
-,('1234ag!','$2a$10$yHQu1zLRjDr6JN3adFqi1.T.sfCnz2uWrndvNh5kVOUe1E1Ekfup.','2022-12-01 17:53:04','2022-12-01 17:53:04',0,NULL)
-,('tom','$2a$10$qlu40iC7w/28F2jXgqJz/OLg9ZpX8Cr/kEeWwkRHm6nGJQXcbCi4C','2022-12-01 17:55:13','2022-12-01 17:55:13',0,NULL)
+('tom','$2a$10$qlu40iC7w/28F2jXgqJz/OLg9ZpX8Cr/kEeWwkRHm6nGJQXcbCi4C','2022-12-01 17:55:13','2022-12-01 17:55:13',0,NULL)
 ;
